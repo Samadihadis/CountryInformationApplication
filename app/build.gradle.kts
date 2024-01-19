caa53ac10884e6f8041e322bf03fb69eed7bcf9a
Hadis Samadi <samadi.hadis@gmail.com>
2024-01-19 16:11:21 +0330

    Implementation GSON and Okhttp and Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,12 +42,19 @@ android {
 dependencies {
 
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.code.gson:gson:2.10.1")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.1.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 
     testImplementation("junit:junit:4.13.2")
 
